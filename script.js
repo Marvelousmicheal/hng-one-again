36d03c2406c85f9b5e5aea283dfdfa622f86574b
marvelous micheal <donatusmarvelous09@gmail.com>
2023-09-10 08:35:41 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+// const todaysDay = document.querySelector(".days-of-week")
+// const currentDate = new Date()
+
+
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+//     const Day = daysOfWeek[currentDate.getDay()]
+    
+//     todaysDay.textContent = Day;
+    
+//     console.log("today is " + Day)
+// })
+
+document.addEventListener("DOMContentLoaded", function () {
+    const todaysDay = document.querySelector(".days-of-week");
+    const timeDiv = document.querySelector(".time-of-the-day")
+    const currentDate = new Date();
+const currentTime = new Date()
+
+
+
+    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const Day = daysOfWeek[currentDate.getDay()];
+    todaysDay.textContent = `Today is ${Day}` ;
+
+    /////////////////////////////////
+
+    const hours = currentTime.getHours()
+    const minutes = currentTime.getMinutes()
+    const seconds = currentTime.getSeconds()
+
+    const time = `${hours}:${minutes} : ${seconds}`
+
+    timeDiv.textContent = `The Time is ${time}`
+
+
+   
+});
